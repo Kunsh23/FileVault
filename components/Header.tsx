2ede08d366d1d73610196ea3f5c1584bb2d9ff15
Kunsh21cs1014 <21cs1014@mvn.edu.in>
2024-12-07 16:06:55 +0530

    eight commit

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Button } from './ui/button'
+import Image from 'next/image'
+import Search from './Search'
+import FileUploader from './FileUploader'
+
+interface Props {
+  userId: string;
+  accountId: string;
+}
+
+const Header = ({userId, accountId}: Props) => {
+  return (
+    <header className='header'>
+        <Search />
+
+        <div className='header-wrapper'>
+            <FileUploader ownerId={userId} accountId={accountId} />
+
+            <form action="">
+                <Button type="submit" className='sign-out-button'>
+                    <Image src="/assets/icons/logout.svg" alt="logo" width={24} height={24} className='w-6' />
+                </Button>
+            </form>
+        </div>
+    </header>
+  )
+}
+
+export default Header
